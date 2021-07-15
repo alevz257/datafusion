c9c8d8ced9054508e258c345f8ab289c9eed410b
alevz <alevz@google.com>
2021-07-15 14:36:21 +0000

    test

--- curl.sh
+++ curl.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-curl -X PUT -H "Authorization: Bearer ${AUTH_TOKEN}" "${CDF_API}/v3/namespaces/default/apps/DataFusionQuickstart" -d "@./DataFusionQuickstart-cdap-data-pipeline.json"
+export AUTH_TOKEN=$(gcloud auth print-access-token)
+export CDAP_ENDPOINT=$(gcloud beta data-fusion instances describe --location=asia-southeast2 --format="value(apiEndpoint)" alevz-fusion)
+curl -X PUT -H "Authorization: Bearer ${AUTH_TOKEN}" "${CDF_API}/v3/namespaces/default/apps/DataFusionQuickstart" -d "@./DataFusionQuickstart-cdap-data-pipeline.json""
+
