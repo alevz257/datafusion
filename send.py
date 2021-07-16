2d7b2da9496b75ab593fb69426a68042d49b7d3c
alevz <alevz@google.com>
2021-07-16 07:24:35 +0000

    cleanup

--- send.py
+++ send.py
@@ -6,12 +6,13 @@ print(os.environ['CDAP_ENDPOINT'])
 
 urlSuffix = '/v3/namespaces/default/apps/DataFusionQuickstart'
 url = os.environ['CDAP_ENDPOINT'] + urlSuffix
-print(url)
+#print(url)
 
 tokenAuth = 'Bearer ' + os.environ['AUTH_TOKEN']
-print(tokenAuth)
+#print(tokenAuth)
 
 headers = {'Content-Type': 'application/json', 'Authorization': tokenAuth }
 print(headers)
 
 r = requests.put(url, data=open('pipeline.json', 'rb'), headers=headers)
+print('finished uploading pipeline.json to datafusion')
